bcc63420b46e3d23c342fb9aeb28032c551d0120
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2013-10-09 23:03:24 +0200

    Begin writing down the code for data migration.

---
+++ src/qlg_migrate.erl
@@ -0,0 +1,50 @@
+-module(qlg_migrate).
+
+-export([migrate/0]).
+
+-define(HOSTNAME, "localhost").
+-define(USERNAME, "qlglicko_processing").
+-define(PASSWORD, "0okTivlur7").
+
+migrate() ->
+	Conn = connect(),
+	Prepare = prepare_statements(Conn),
+	loop(Conn, Prepare),
+	disconnect(Conn).
+	
+disconnect(Conn) ->
+	pgsql:close(Conn).
+	
+connect() ->
+	{ok, Conn} = pgsql:connect(?HOSTNAME, ?USERNAME, ?PASSWORD, [{database, "qlglicko"}]),
+	Conn.
+
+loop(Conn, {QueryStmt}) ->
+	run_loop(Conn, QueryStmt, pgsql:execute(Conn, QueryStmt, query_portal, 10*1000)).
+	
+run_loop(Conn, QueryStmt, {partial, Rows}) ->
+	[convert_row(Conn, R) || R <- Rows],
+	run_loop(Conn, QueryStmt, pgsql:execute(Conn, QueryStmt, query_portal, 10*1000));
+run_loop(Conn, _QueryStmt, {ok, Rows}) ->
+	[convert_row(Conn, R) || R <- Rows],
+	ok.
+
+prepare_statements(Conn) ->
+	{ok, QueryStmt} = 
+		pgsql:parse(
+			Conn, query_raw_match,
+			"SELECT id, content FROM raw_match", []),
+	ok = pgsql:bind(Conn, QueryStmt, query_portal, []),
+	{QueryStmt}.
+
+convert_row(Conn, [Id, Content]) ->
+	Term = binary_to_term(Content),
+	insert(Conn, Id, jsx:to_binary(Term)).
+
+insert(Conn, Id, JSON) ->
+	ok = pgsql:equery(
+		Conn,
+		"INSERT INTO core.raw_match_json (id, content) VALUES ($1, $2)",
+		[Id, JSON]).
+		
+
