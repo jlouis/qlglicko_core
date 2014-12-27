1b12468a2ef75d6f9b7e3e3f320c903371b0a713
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2014-12-27 15:55:12 +0100

    Correct the return value of store_match_json.

--- src/qlg_db.erl
+++ src/qlg_db.erl
@@ -36,7 +36,8 @@ tournament_stats(Tourney, Count) ->
 
 %% @todo delete this one once all matches have been moved
 store_match_json(ID, Added, Content, Analyzed) ->
-    equery(processing, "SELECT processing.store_match_json($1 :: uuid, $2 :: timestamp, $3 :: jsonb, $4 :: boolean)" , [ID, Added, Content, Analyzed]).
+    {ok, _, _} = equery(processing, "SELECT processing.store_match_json($1 :: uuid, $2 :: timestamp, $3 :: jsonb, $4 :: boolean)" , [ID, Added, Content, Analyzed]),
+    ok.
 
 %% @todo delete this one once all matches have been moved
 mark_moved(ID) ->
