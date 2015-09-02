bd71ef37f69a125c3e5df70872598061407c8587
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-09-02 16:02:27 +0200

    Use Release 18.0 time calculations in the code base.

--- src/ql_fetch.erl
+++ src/ql_fetch.erl
@@ -72,18 +72,18 @@ alive_url(Name) ->
       ["http://www.quakelive.com/profile/summary", Name], "/").
 
 request(URL) ->
-    From = now(),
+    From = erlang:monotonic_time(),
     case hackney:request(get, URL, [], <<>>, [{pool, default}]) of
         {ok, 200, _, ClientRef} ->
           {ok, Body} = hackney:body(ClientRef),
-          To = now(),
-          case overload(From, To) of
-          	true ->
-          	  ok = lager:info("Overload at QL Site"),
-          	  qlg_overload:overload(),
-          	  ok;
-          	false ->
-          	  ok
+          To = erlang:monotonic_time(),
+          case erlang:convert_time_unit(To - From, native, milli_seconds) of
+              L when L > 6000  ->
+                  ok = lager:info("Overload at QL Site"),
+                  qlg_overload:overload(),
+                  ok;
+              _Otherwise ->
+              ok
           end,
           hackney:close(ClientRef),
           {ok, Body};
@@ -104,12 +104,3 @@ request(URL) ->
             qlg_overload:timeout(),
             {error, Reason}
     end.
-
-overload(From, To) ->
-    calc_interval(From, To) > 6000.
-
-calc_interval({FMega, FSec, FMicro} = F, {TMega, TSec, TMicro} = T)
-  when F < T ->
-    FMs = (FMega * 1000000 + FSec) * 1000 + (FMicro / 1000),
-    TMs = (TMega * 1000000 + TSec) * 1000 + (TMicro / 1000),
-    TMs - FMs.
